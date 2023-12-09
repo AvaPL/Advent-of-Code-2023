33dbdbe885ca200cde0c7ee51e9c6b4abd60092b
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2023-12-09 20:15:07 +0100

    Add day 8 part 2 solution.

---
+++ src/main/scala/day8/Puzzle2.scala
@@ -0,0 +1,54 @@
+package io.github.avapl
+package day8
+
+import scala.annotation.tailrec
+
+@main def puzzle2(): Unit = {
+  val (directions, network) = PuzzleInputParser.parsedInput
+  val startNodes = determineStartNodes(network)
+  val stepsStartToEnd = startNodes.map(countStepsStartToEnd(_, directions, network))
+  val result = leastCommonMultiple(stepsStartToEnd) // I'm disappointed that this works :(
+  println(result)
+}
+
+private def determineStartNodes(network: Network) =
+  network.collect {
+    case (node, _) if node.endsWith(startNodePostfix) => node
+  }.toList
+
+private def countStepsStartToEnd(node: Node, directions: Seq[Direction], network: Network): Long = {
+  var stepsCount = 0L
+
+  @tailrec
+  def loop(currentNode: Node, remainingDirections: List[Direction]): Long =
+    if (currentNode.endsWith(endNodeSuffix))
+      stepsCount
+    else
+      remainingDirections match
+        case direction :: tail =>
+          stepsCount += 1
+          val (leftNode, rightNode) = network(currentNode)
+          direction match
+            case Left  => loop(leftNode, tail)
+            case Right => loop(rightNode, tail)
+        case Nil => loop(currentNode, remainingDirections = directions.toList)
+
+  loop(currentNode = node, remainingDirections = directions.toList)
+}
+
+@tailrec
+private def leastCommonMultiple(longs: List[Long]): Long =
+  longs match
+    case first :: second :: tail => leastCommonMultiple(leastCommonMultiple(first, second) :: tail)
+    case first :: Nil            => first
+
+private def leastCommonMultiple(first: Long, second: Long) =
+  first / greatestCommonDivisor(first, second) * second
+
+@tailrec
+private def greatestCommonDivisor(first: Long, second: Long): Long =
+  if (second == 0) first
+  else greatestCommonDivisor(second, first % second)
+
+private lazy val startNodePostfix = "A"
+private lazy val endNodeSuffix = "Z"
